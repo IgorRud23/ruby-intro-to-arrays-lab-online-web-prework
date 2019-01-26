eb391015fe77998ee276069fc95b3cbfec85b435
Igor Rud <rud.igor23@gmail.com>
2019-01-26 23:12:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,7 @@ def instantiate_new_array
   @my_new_array = []
 end
 
-def instance_new_array
+def array_with_two_elements
   @my_new_array = ["dog", "cat", "mouse", "Tom", "Jerry"]
   @my_new_array = ["dog", "cat"]
 end
